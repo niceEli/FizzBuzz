9d4a68e2e2a7c85fcfb4b35b74c0d430c2ad2883
Eli <78963198+niceEli@users.noreply.github.com>
2023-11-27 12:08:17 -0500

    make even better

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
-var strings = [];
+let strings:Array<string> = [];
+let answers:number = 100;
 
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < answers; i++) {
     strings.push((i+1).toString());
 }
 
 for (let i = 0; i < strings.length; i++) {
     const element = strings[i];
     const felement = Number(element);
-    if (felement % 3 == 0 && felement % 5 == 0){
+    if (felement % 3 === 0 && felement % 5 == 0){
         strings.splice(i, 1, "FizzBuzz");
-    } else if (felement % 3 == 0) {
+    } else if (felement % 3 === 0) {
         strings.splice(i, 1, "Fizz");
-    } else if(felement % 5 == 0){
+    } else if(felement % 5 === 0){
         strings.splice(i, 1, "Buzz");
     }
 }
